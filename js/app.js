e01ba8141137443cdd2cfe3726e3df45cc8a5323
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2026-01-02 21:56:08 +0100

    feat: conectar validación del formulario con la función mostrarAlerta

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas');
+const monedaSelect = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
+
+const objBusqueda = {
+    moneda: '',
+    criptomoneda: ''
+}
 
 // Crear un Promise
 const obtenerCriptomonedas = criptomonedas => new Promise( resolve => {
@@ -7,6 +14,11 @@ const obtenerCriptomonedas = criptomonedas => new Promise( resolve => {
 
 document.addEventListener('DOMContentLoaded', () => {
     consultarCriptomonedas();
+
+    formulario.addEventListener('submit', submitFormulario);
+
+    criptomonedasSelect.addEventListener('change', leerValor);
+    monedaSelect.addEventListener('change', leerValor);
 })
 
 function consultarCriptomonedas() {
@@ -22,10 +34,30 @@ function selectCriptomonedas(criptomonedas) {
     criptomonedas.forEach(cripto => {
         const {FullName, Name} = cripto.CoinInfo;
 
-        const option =document.createElement('option');
+        const option = document.createElement('option');
         option.value = Name;
         option.textContent = FullName;
 
         criptomonedasSelect.appendChild(option);
     })
+}
+
+function leerValor(e) {
+    objBusqueda[e.target.name] = e.target.value;
+}
+
+function submitFormulario(e) {
+    e.preventDefault();
+
+    // Validar
+    const{moneda, criptomoneda} = objBusqueda;
+
+    if(moneda === '' || criptomoneda === '') {
+        mostrarAlerta('Ambos campos son obligatorios');
+        return;
+    }
+}
+
+function mostrarAlerta(msg) {
+    console.log(msg);
 }
